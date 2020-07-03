e176126a6769f8676e81a2ec9812f1032209d9aa
Jacano <641283231@qq.com>
2020-07-03 10:07:03 +0800

    docs:添加热刷新注释

--- src/chrome/background/hmr.js
+++ src/chrome/background/hmr.js
@@ -17,11 +17,13 @@ const filesInDirectory = dir =>
     })
   );
 
+// 遍历插件目录，读取文件信息，组合文件名称和修改时间成数据
 const timestampForFilesInDirectory = dir =>
   filesInDirectory(dir).then(files =>
     files.map(f => f.name + f.lastModifiedDate).join()
   );
 
+// 刷新当前活动页
 const reload = () => {
   window.chrome.tabs.query(
     {
@@ -33,16 +35,20 @@ const reload = () => {
       if (tabs[0]) {
         window.chrome.tabs.reload(tabs[0].id);
       }
+      // 强制刷新页面
       window.chrome.runtime.reload();
     }
   );
 };
 
+// 观察文件改动
 const watchChanges = (dir, lastTimestamp) => {
   timestampForFilesInDirectory(dir).then(timestamp => {
+    // 文件没有改动则循环监听watchChanges方法
     if (!lastTimestamp || lastTimestamp === timestamp) {
       setTimeout(() => watchChanges(dir, timestamp), 1000); // retry after 1s
     } else {
+      // 强制刷新页面
       reload();
     }
   });
@@ -51,6 +57,7 @@ const watchChanges = (dir, lastTimestamp) => {
 const installReload = () => {
   window.chrome.management.getSelf(self => {
     if (self.installType === "development") {
+      // 获取插件目录，监听文件变化
       window.chrome.runtime.getPackageDirectoryEntry(dir => watchChanges(dir));
     }
   });
